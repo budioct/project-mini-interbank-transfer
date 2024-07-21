0e65ad2ef99950a491517c81347586a895963fb7
Budhi Octaviansyah <budioct@gmail.com>
2024-07-21 08:30:59 +0700

    start project

---
+++ src/main/java/dev/budhi/BankTransferApplication.java
@@ -0,0 +1,13 @@
+package dev.budhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankTransferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/budhi/BankTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.budhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
